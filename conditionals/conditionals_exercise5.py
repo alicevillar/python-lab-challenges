4132d9c6cbcf08ada22bcee43700abec145fdc9d
alicevillar <av20678@essx.ac.uk>
2021-05-21 16:23:23 -0300

    conditional exercises

--- conditionals/conditionals_exercise5.py
+++ conditionals/conditionals_exercise5.py
@@ -1,29 +1,5 @@
 
 #######################################################################################################
-#                               Conditionals - Lab Exercise 4
-#
-#  Use the variable x as you write this program. x will represent a string.
-#  Write a program using the elif keyword  that determines if x is a primary color (red, blue, or yellow).
-#  If yes, print _ is primary color, where the blank is the value of x.
-#  If no, print _ is not a primary color, where the blank is the value of x.
-#
-#   Expected Output:
-#   If x is red, then the output would be: red is a primary color.
-#   If x is teal, then the output would be: teal is not a primary color.
-#
-#######################################################################################################
-
-x='blue'
-if x == "red":
-    print("x is red is a primary color")
-elif x == "blue":
-    print("blue is a primary color")
-elif x == "yellow":
-    print("yellow is a primary color")
-
-# Output => blue is a primary color
-
-#######################################################################################################
 #                                 Conditionals -  Lab Exercise 5
 #
 #  Use the variable x as you write this program. x will represent a string.
