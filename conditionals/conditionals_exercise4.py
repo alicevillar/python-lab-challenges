bd325397e60cceb1e7aff256041cfcea2dcc2a9c
alicevillar <av20678@essx.ac.uk>
2021-05-22 11:15:33 -0300

    update

--- conditionals/conditionals_exercise4.py
+++ conditionals/conditionals_exercise4.py
@@ -1,24 +1,42 @@
 
 #######################################################################################################
-#                               Conditionals - Lab Exercise 4
+#                                 Conditionals -  Lab Exercise 5
 #
 #  Use the variable x as you write this program. x will represent a string.
-#  Write a program using the elif keyword  that determines if x is a primary color (red, blue, or yellow).
-#  If yes, print _ is primary color, where the blank is the value of x.
-#  If no, print _ is not a primary color, where the blank is the value of x.
+#  Write a program that determines if x is a vowel (a, e, i, o, and u ).
+#  If yes, print _ is a vowel, where the blank is the value of x.
+#  If no, print _ is not a vowel, where the blank is the value of x.
 #
 #   Expected Output:
-#   If x is red, then the output would be: red is a primary color.
-#   If x is teal, then the output would be: teal is not a primary color.
+#   If x is a, then the output would be: a is a vowel.
+#   If x is z, then the output would be: z is not a vowel.
 #
 #######################################################################################################
 
-x='blue'
-if x == "red":
-    print("x is red is a primary color")
-elif x == "blue":
-    print("blue is a primary color")
-elif x == "yellow":
-    print("yellow is a primary color")
+x='a'
+if x in 'aeiouAEIOU':
+    print(x, "is a vowel")
+else:
+    print(x, "is not a vowel")
 
-# Output => blue is a primary color
+# Output => a is a vowel
+
+x='z'
+if x in 'aeiouAEIOU':
+    print(x, "is a vowel")
+else:
+    print(x, "is not a vowel")
+
+# Output => z is not a vowel
+
+#######################################################################################################################
+# ALTERNATIVE SOLUTION:
+#######################################################################################################################
+
+x='a'
+if x == "a" or x == "e" or x =="i" or x == "o" or x == "u" or x == "A" or x == "E" or x =="I" or x == "O" or x == "U":
+    print(x + " is a vowel")
+else:
+    print(x + " is not a vowel")
+
+# Output => a is a vowel
